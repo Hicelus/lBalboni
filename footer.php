19b576a9a749db80edecb501bdef2f7159eb39f5
Hicelus <ls.balboni@outlook.com>
2020-07-03 01:09:24 -0300

    0.1

---
+++ footer.php
@@ -0,0 +1,5 @@
+    <script src="assets/js/jquery-3.5.1.js" class="text-danger"></script>
+    <script src="assets/js/popper.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+</body>
+</html>
